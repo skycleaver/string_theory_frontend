c9ca5351f4e86fe461d50875a899305bbaa4bbd2
Pau <pau.ortega.puig@gmail.com>
2017-03-29 13:11:06 +0200

    Extract numeric variables from draw_strings.js

--- js/draw_strings.js
+++ js/draw_strings.js
@@ -1,77 +1,87 @@
 canvas_margin = 50;
+string_separation = 50;
+fretboard_length = 1000;
+fret_length = 80;
+number_of_strings = 6;
+fretboard_width = string_separation * (number_of_strings - 1);
+mark_radius = 10;
+note_margin_from_fret = 30;
+note_margin_from_string = 5;
 
 my_canvas = document.getElementById('canvas');
 context = my_canvas.getContext('2d');
 context.beginPath();
 
 drawFretboard();
-drawString(0);
-drawString(50);
-drawString(100);
-drawString(150);
-drawString(200);
-drawString(250);
+drawStrings();
 drawChord();
-
-function drawString(height) {
-	context.moveTo(0 + canvas_margin, height + canvas_margin);
-	context.lineTo(1000 + canvas_margin, height + canvas_margin);
-	context.stroke();
-}
 
 function drawFretboard() {
 	for (var i = 0; i < 12; i++) {
-		drawFret(80 * i);
+		drawFret(fret_length * i);
 		if (i === 3) {
-			drawMark(80 * i - 40);
+			drawMark(fret_length * i - fret_length/2);
 		}
 		if (i === 5) {
-			drawMark(80 * i - 40);
+			drawMark(fret_length * i - fret_length/2);
 		}
 		if (i === 7) {
-			drawMark(80 * i - 40);
+			drawMark(fret_length * i - fret_length/2);
 		}
 	}
 }
 
+function drawStrings() {
+	for (var i = 0; i < number_of_strings; i++) {
+		drawString(string_separation * i);
+	}
+}
+
+function drawString(height) {
+	context.moveTo(0 + canvas_margin, height + canvas_margin);
+	context.lineTo(fretboard_length + canvas_margin, height + canvas_margin);
+	context.stroke();
+}
+
 function drawFret(width) {
 	context.moveTo(width + canvas_margin, 0 + canvas_margin);
-	context.lineTo(width + canvas_margin, 250 + canvas_margin);
+	context.lineTo(width + canvas_margin, fretboard_width + canvas_margin);
 	context.stroke();
 }
 
 function drawMark(width) {
 	context.beginPath();
-	context.arc(width + canvas_margin, 125 + canvas_margin, 10, 0, 2*Math.PI);
+	context.arc(width + canvas_margin, fretboard_width/2 + canvas_margin, mark_radius, 0, 2*Math.PI);
 	context.stroke();
 }
 
 function drawChord() {
 	for (var i = 0; i < 12; i++) {
 		if (isFretValid(chord[5][i])) {
-			drawNote(80 * i - 20, 0, chord[5][i]);
+			drawNote(fret_length * i, string_separation * 0, chord[5][i]);
 		}
 		if (isFretValid(chord[4][i])) {
-			drawNote(80 * i - 20, 50, chord[4][i]);
+			drawNote(fret_length * i, string_separation * 1, chord[4][i]);
 		}
 		if (isFretValid(chord[3][i])) {
-			drawNote(80 * i - 20, 100, chord[3][i]);
+			drawNote(fret_length * i, string_separation * 2, chord[3][i]);
 		}
 		if (isFretValid(chord[2][i])) {
-			drawNote(80 * i - 20, 150, chord[2][i]);
+			drawNote(fret_length * i, string_separation * 3, chord[2][i]);
 		}
 		if (isFretValid(chord[1][i])) {
-			drawNote(80 * i - 20, 200, chord[1][i]);
+			drawNote(fret_length * i, string_separation * 4, chord[1][i]);
 		}
 		if (isFretValid(chord[0][i])) {
-			drawNote(80 * i - 20, 250, chord[0][i]);
+			drawNote(fret_length * i, string_separation * 5, chord[0][i]);
 		}
 	}
 }
 
 function drawNote(width, height, note) {
-	width = width - 10 + canvas_margin;
-	height = height + 5 + canvas_margin;
+	width = width - note_margin_from_fret + canvas_margin;
+	height = height + note_margin_from_string + canvas_margin;
+	
 	context.font = 'bold 20px Verdana';
 	context.fillStyle = 'red';
 	context.fillText(note, width, height);
