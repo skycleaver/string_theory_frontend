702f43186001d0104e45a3642a271a1d8e3eeacc
Pau <pau.ortega.puig@gmail.com>
2017-04-20 12:35:00 +0200

    Quickfix of 7th selector with '-' value

--- js/chords/click_chord.js
+++ js/chords/click_chord.js
@@ -1,7 +1,7 @@
 function clickChord(chord_root, chord_type) {
 	$('select[name=chord_root]').val(chord_root);
 	$('select[name=chord_type]').val(chord_type);
-	$('select[name=chord_seventh]').val('-');
+	$('select[name=chord_seventh]').val('');
 	// triggering the change on any of them performs the call
 	$('select[name=chord_type]').trigger("change");
 }
